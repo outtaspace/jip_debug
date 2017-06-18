33c3579ce9d63405a43b20bc26cf6b7a49797566
outtaspace <flyweight@yandex.ru>
2017-06-18 15:05:31 +0300

    Small changes.

--- lib/JIP/Debug.pm
+++ lib/JIP/Debug.pm
@@ -5,24 +5,23 @@ use base qw(Exporter);
 use 5.006;
 use strict;
 use warnings;
-use English qw(-no_match_vars);
-use Fcntl qw(LOCK_EX LOCK_UN);
-use Data::Dumper qw(Dumper);
 use Carp qw(croak);
+use Data::Dumper qw(Dumper);
+use Fcntl qw(LOCK_EX LOCK_UN);
+use English qw(-no_match_vars);
 
-our $VERSION   = '0.01';
-our @EXPORT_OK = qw(to_debug to_debug_raw);
+our $VERSION   = '0.999_001';
+our @EXPORT_OK = qw(to_debug to_debug_raw to_debug_empty);
 
-our $COLOR = 'bright_yellow';
+our $HANDLE = \*STDERR;
 
-our $MSG_FORMAT = qq{%s\n%s\n\n%s};
-
-our $MSG_DELIMITER = q{=} x 80 . qq{\n\n};
+our $MSG_FORMAT    = qq{%s\n%s\n%s\n%s\n\n};
+our $MSG_DELIMITER = q{-} x 80;
 
 our $DUMPER_INDENT   = 1;
 our $DUMPER_DEEPCOPY = 1;
 
-our $HANDLE = \*STDERR;
+our $COLOR = 'bright_green';
 
 our $MAYBE_COLORED = sub { $ARG[0] };
 eval {
@@ -37,7 +36,7 @@ our $MAKE_MSG_HEADER = sub {
     # $MAKE_MSG_HEADER=0, to_debug=1, subroutine=2
     my $subroutine = (caller(2))[3];
 
-    $subroutine = resolve_subroutine_name($subroutine);
+    $subroutine = _resolve_subroutine_name($subroutine);
 
     my $text = join q{, }, (
         sprintf('package=%s', $package),
@@ -68,26 +67,46 @@ sub to_debug {
         Dumper(\@_);
     };
 
-    my $msg = sprintf $MSG_FORMAT,
-        $MAKE_MSG_HEADER->(),
-        $msg_body,
-        $MAYBE_COLORED->(defined $MSG_DELIMITER ? $MSG_DELIMITER : q{});
+    my $msg_delimiter = defined $MSG_DELIMITER ? $MSG_DELIMITER : q{};
+    $msg_delimiter = $MAYBE_COLORED->($msg_delimiter);
 
-    send_to_output($msg);
+    my $msg = sprintf $MSG_FORMAT,
+        $msg_delimiter,
+        $MAKE_MSG_HEADER->(),
+        $msg_delimiter,
+        $msg_body;
+
+    _send_to_output($msg);
 }
 
 sub to_debug_raw {
     my $msg_text = shift;
 
-    my $msg = sprintf $MSG_FORMAT,
-        $MAKE_MSG_HEADER->(),
-        $msg_text,
-        $MAYBE_COLORED->(defined $MSG_DELIMITER ? $MSG_DELIMITER : q{});
+    my $msg_delimiter = defined $MSG_DELIMITER ? $MSG_DELIMITER : q{};
+    $msg_delimiter = $MAYBE_COLORED->($msg_delimiter);
 
-    send_to_output($msg);
+    my $msg = sprintf $MSG_FORMAT,
+        $msg_delimiter,
+        $MAKE_MSG_HEADER->(),
+        $msg_delimiter,
+        $msg_text;
+
+    _send_to_output($msg);
 }
 
-sub send_to_output {
+sub to_debug_empty {
+    my $msg_delimiter = defined $MSG_DELIMITER ? $MSG_DELIMITER : q{};
+    $msg_delimiter = $MAYBE_COLORED->($msg_delimiter);
+
+    my $msg = sprintf qq{%s\n%s%s},
+        $msg_delimiter,
+        $msg_delimiter,
+        qq{\n} x 20;
+
+    _send_to_output($msg);
+}
+
+sub _send_to_output {
     my $msg = shift;
 
     return unless $HANDLE;
@@ -99,7 +118,7 @@ sub send_to_output {
     return 1;
 }
 
-sub resolve_subroutine_name {
+sub _resolve_subroutine_name {
     my $subroutine = shift;
 
     return unless defined $subroutine;
@@ -110,4 +129,85 @@ sub resolve_subroutine_name {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+JIP::Debug - provides a convenient way to attach debug print statements anywhere in a program.
+
+=head1 VERSION
+
+Version 0.999_001
+
+=head1 SYNOPSIS
+
+    use JIP::Debug qw(to_debug to_debug_raw to_debug_empty);
+
+    # The to_debug and to_debug_raw functions print messages to an output stream.
+
+    # For complex data structures (references, arrays and hashes) you can use the
+    to_debug(
+        an_array    => [],
+        a_hash      => {},
+        a_reference => \42,
+    );
+
+    # Prints a string
+    to_debug_raw('Hello');
+
+    # Prints empty lines
+    to_debug_empty();
+
+=head1 SEE ALSO
+
+Debug::Simple, Debuggit, Debug::Easy
+
+=head1 AUTHOR
+
+Vladimir Zhavoronkov, C<< <flyweight at yandex.ru> >>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2017 Vladimir Zhavoronkov.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the the Artistic License (2.0). You may obtain a
+copy of the full license at:
+
+L<http://www.perlfoundation.org/artistic_license_2_0>
+
+Any use, modification, and distribution of the Standard or Modified
+Versions is governed by this Artistic License. By using, modifying or
+distributing the Package, you accept this license. Do not use, modify,
+or distribute the Package, if you do not accept this license.
+
+If your Modified Version has been derived from a Modified Version made
+by someone other than you, you are nevertheless required to ensure that
+your Modified Version complies with the requirements of this license.
+
+This license does not grant you the right to use any trademark, service
+mark, tradename, or logo of the Copyright Holder.
+
+This license includes the non-exclusive, worldwide, free-of-charge
+patent license to make, have made, use, offer to sell, sell, import and
+otherwise transfer the Package with respect to any patent claims
+licensable by the Copyright Holder that are necessarily infringed by the
+Package. If you institute patent litigation (including a cross-claim or
+counterclaim) against any party alleging that the Package constitutes
+direct or contributory patent infringement, then this Artistic License
+to you shall terminate on the date that such litigation is filed.
+
+Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
+AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
+THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
+YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
+CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
+CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+=cut
+
 
--- t/00-jip-debug.t
+++ t/00-jip-debug.t
@@ -1,7 +1,5 @@
 #!/usr/bin/env perl
 
-use lib 'lib'; # FIXME
-
 use 5.006;
 use strict;
 use warnings FATAL => 'all';
@@ -9,7 +7,12 @@ use Test::More;
 use English qw(-no_match_vars);
 use Capture::Tiny qw(capture capture_stderr);
 
-plan tests => 7;
+BEGIN {
+    eval "use Test::Exception";
+    plan skip_all => "Test::Exception needed" if $@;
+}
+
+plan tests => 8;
 
 subtest 'Require some module' => sub {
     plan tests => 2;
@@ -26,20 +29,21 @@ subtest 'Require some module' => sub {
 };
 
 subtest 'Exportable functions' => sub {
-    plan tests => 3;
+    plan tests => 4;
 
-    can_ok 'JIP::Debug', qw(to_debug to_debug_raw);
+    can_ok 'JIP::Debug', qw(to_debug to_debug_raw to_debug_empty);
 
-    eval { to_debug() } or do {
-        like $EVAL_ERROR, qr{
-            Undefined \s subroutine \s &main::to_debug \s called
-        }x;
-    };
-    eval { to_debug_raw() } or do {
-        like $EVAL_ERROR, qr{
-            Undefined \s subroutine \s &main::to_debug_raw \s called
-        }x;
-    };
+    throws_ok { to_debug() } qr{
+        Undefined \s subroutine \s &main::to_debug \s called
+    }x;
+
+    throws_ok { to_debug_raw() } qr{
+        Undefined \s subroutine \s &main::to_debug_raw \s called
+    }x;
+
+    throws_ok { to_debug_empty() } qr{
+        Undefined \s subroutine \s &main::to_debug_empty \s called
+    }x;
 };
 
 subtest 'Exportable variables' => sub {
@@ -47,8 +51,8 @@ subtest 'Exportable variables' => sub {
 
     no warnings qw(once);
 
-    ok $JIP::Debug::COLOR           eq 'bright_yellow';
-    ok $JIP::Debug::MSG_DELIMITER   eq q{=} x 80 . qq{\n\n};
+    ok $JIP::Debug::COLOR           eq 'bright_green';
+    ok $JIP::Debug::MSG_DELIMITER   eq q{-} x 80;
     ok $JIP::Debug::DUMPER_INDENT   eq 1;
     ok $JIP::Debug::DUMPER_DEEPCOPY eq 1;
 
@@ -57,29 +61,29 @@ subtest 'Exportable variables' => sub {
     ok ref($JIP::Debug::MAKE_MSG_HEADER) eq 'CODE';
 };
 
-subtest 'resolve_subroutine_name()' => sub {
+subtest '_resolve_subroutine_name()' => sub {
     plan tests => 5;
 
-    is JIP::Debug::resolve_subroutine_name(),                  undef;
-    is JIP::Debug::resolve_subroutine_name(undef),             undef;
-    is JIP::Debug::resolve_subroutine_name(q{}),               undef;
-    is JIP::Debug::resolve_subroutine_name('subroutine_name'), undef;
+    is JIP::Debug::_resolve_subroutine_name(),                  undef;
+    is JIP::Debug::_resolve_subroutine_name(undef),             undef;
+    is JIP::Debug::_resolve_subroutine_name(q{}),               undef;
+    is JIP::Debug::_resolve_subroutine_name('subroutine_name'), undef;
 
-    is JIP::Debug::resolve_subroutine_name('package::package::subroutine_name'), 'subroutine_name';
+    is JIP::Debug::_resolve_subroutine_name('package::package::subroutine_name'), 'subroutine_name';
 };
 
-subtest 'send_to_output()' => sub {
+subtest '_send_to_output()' => sub {
     plan tests => 4;
 
     my ($stdout, $stderr) = capture {
-        JIP::Debug::send_to_output(42);
+        JIP::Debug::_send_to_output(42);
     };
     is $stderr, 42;
     is $stdout, q{};
 
     local $JIP::Debug::HANDLE = \*STDOUT;
     ($stdout, $stderr) = capture {
-        JIP::Debug::send_to_output(42);
+        JIP::Debug::_send_to_output(42);
     };
     is $stderr, q{};
     is $stdout, 42;
@@ -99,11 +103,14 @@ subtest 'to_debug()' => sub {
     };
     like $stderr_listing, qr{
         ^
+        delimiter
+        \n
         header
+        \n
+        delimiter
         \n
         \$VAR1\s+=\s+\[42\];
         \n\n
-        delimiter
         $
     }x;
 };
@@ -122,13 +129,37 @@ subtest 'to_debug_raw()' => sub {
     };
     like $stderr_listing, qr{
         ^
+        delimiter
+        \n
         header
+        \n
+        delimiter
         \n
         42
         \n\n
-        delimiter
         $
     }x;
+};
 
+subtest 'to_debug_empty()' => sub {
+    plan tests => 1;
+
+    my $stderr_listing = capture_stderr {
+        local $JIP::Debug::MAKE_MSG_HEADER = sub { 'header' };
+        local $JIP::Debug::MAYBE_COLORED   = sub { $ARG[0] };
+
+        local $JIP::Debug::DUMPER_INDENT = 0;
+        local $JIP::Debug::MSG_DELIMITER = 'delimiter';
+
+        JIP::Debug::to_debug_empty(42);
+    };
+    like $stderr_listing, qr{
+        ^
+        delimiter
+        \n
+        delimiter
+        \n{20}
+        $
+    }x;
 };
 
